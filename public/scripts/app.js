60eceb6630ac98a099336c26d70dea540bcec4e3
Cory Fauver <cofauver@gmail.com>
2016-02-24 21:56:12 -0800

    adding more movies to the search results is now possible.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,17 @@
 var input = document.getElementById('movie-input');
+var moreResultsButton = document.getElementById('more-results');
 var xmlhttp = new XMLHttpRequest();
+var currentQuery, currentQueryPage;
+var currentResults = [];
 
-function displayList(resultsList){
-	resultsList = JSON.stringify(resultsList);
-	document.getElementsByTagName('rbl-repeater')[0].setAttribute('content', resultsList);
-};
 
+/* 
+This function makes the request for resources from the
+Open Movie Database api.
+	query(string, required): the text to search in omdb
+	page(number, not required): the page that you want to get.
+				Requests return pages of 10 results at a time.
+*/ 
 function requestApiSearch(query, page){
 	query = query.replace(' ', '+');
 	var url  = 'http://www.omdbapi.com/?s=' + query;
@@ -16,21 +22,49 @@ function requestApiSearch(query, page){
 	xmlhttp.send();
 };
 
+/* 
+This watches for changes in the state of xmlhttp. When
+the readyState is 4 (request finished and response is ready) 
+and when the status code is 200, we read in the response and
+push the results to the current list of movies.
+*/ 
 xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         var response = JSON.parse(xmlhttp.responseText)
-        // console.log(response); //for checking the details of the API response 
-        var results = response.Search; 
-                			// ^ Search is the name of the attribute in the API response.
-        displayList(results);
+        var newResults = response.Search;
+        						// ^ Search is the name of the attribute containing the movie list in the API response.
+        Array.prototype.push.apply(currentResults, newResults); 
+        /* ^ takes Array's push function and applies it to 
+        currentResults with newResults as the list of arguments.
+        This trick allows us to avoid repeating through all of 
+        new results and pushing them one by one to the currentResults.
+        */
+
+        console.log(currentResults);
+		document.getElementsByTagName('rbl-repeater')[0].setAttribute('content', JSON.stringify(currentResults));
     }
 };
 
+
+/* 
+This listens for changes in the input box. Any change in the text
+will trigger the query data to reset and clear the currentResults.
+Then it requests an api search for the new, changed query.
+*/ 
 input.addEventListener('input', function(){
-	requestApiSearch(input.value);
+	currentQuery = input.value;
+	currentQueryPage = 1;
+	currentResults = [];
+	requestApiSearch(currentQuery);
 });
 
-document.getElementById('more-results').addEventListener('click',function(){
-	
+/* 
+This listens for clicks on the "See more results" button. When clicked,
+it increments the page that is being shown and searches for the next
+ten results (OMDB returns pages of 10 results at a time)
+*/ 
+moreResultsButton.addEventListener('click',function(){
+	currentQueryPage += 1;
+	requestApiSearch(currentQuery, currentQueryPage);
 });
 
